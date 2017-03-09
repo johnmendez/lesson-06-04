676c01f859a51d92fe124de6c9e5204f04631329
Kate Leslie <thisizk8@gmail.com>
2017-03-09 12:16:49 -0600

    List all blog posts

--- app/routes/index.vue
+++ app/routes/index.vue
@@ -1,15 +1,29 @@
 <template lang="html">
   <div class="">
     <h1>All Blog Posts</h1>
+
+    <ul>
+      <li v-for="item in posts">
+        {{item.title}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import store from '../store';
+import { findAll } from '../actions/post';
+
 export default {
   name: 'route-index',
   data() {
     return {
+      posts: this.$select('posts'),
     };
+  },
+
+  created() {
+    store.dispatch(findAll());
   },
 
   methods: {
